0943454a63e9c8abf3a56b4635c832b1b8d81e56
Eisha Fatima Shah <140654347+k200383@users.noreply.github.com>
2025-09-20 23:28:31 +0500

    Add files via upload

---
+++ src/Tests/CorePage.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTest    //new
+{
+    internal class CorePage
+    {
+        public static IWebDriver driver;
+        public static void SeleniumInit(string browser)
+        {
+            if (browser == "Chrome")
+            {
+                var chromeOptions = new ChromeOptions();
+                chromeOptions.AddArguments("--start-maximized");
+                chromeOptions.AddArguments("--incognito");
+                
+                IWebDriver chromeDriver = new ChromeDriver(chromeOptions);
+                driver = chromeDriver;
+            }
+            else if (browser == "Firefox")
+            {
+                FirefoxOptions options = new FirefoxOptions();
+                options.AddArguments("");
+                driver = new FirefoxDriver(options);
+            }
+        }
+
+    }
+}
